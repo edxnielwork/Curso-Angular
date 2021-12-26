9affab1d30425fa3d7985a43b681fd9f708f0dee
edxnielwork <edxnielwork007@gmail.com>
2021-12-25 19:37:25 -0500

    updated

---
+++ typescript-importaciones/src/class/avengers.class.ts
@@ -0,0 +1,13 @@
+
+
+export class Avengers{
+    constructor(
+        public nombre: string,
+        public apellido: string,
+        public edad: number
+    ){}
+
+    mostrar(){
+        console.log(`Hola como estas ${this.nombre} ${this.apellido} y tu edad es: ${this.edad}`);
+    }
+}
--- typescript-importaciones/src/index.ts
+++ typescript-importaciones/src/index.ts
@@ -1,5 +1,10 @@
 
-import { Xmen} from './class/xmen.class';
+//  import { Xmen } from './class/xmen.class';
+//  const wolverine =  new Xmen('Logan', 'Wolverine', 'EDXNIEL');
+//  wolverine.imprimir();
 
-const wolverine =  new Xmen('Logan', 'Wolverine', 'EDXNIEL');
-wolverine.imprimir();
+
+import { Avengers  } from './class/avengers.class';
+
+const spiderman = new Avengers('Daniel', 'Valencia', 17);
+spiderman.mostrar();
