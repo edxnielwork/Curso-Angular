238fbfe63fe7411d9a6c468ae786968d89aab896
edxnielwork <edxnielwork007@gmail.com>
2021-12-28 23:58:16 -0500

    updated

--- PrimeraApp/src/app/components/body/body.component.ts
+++ PrimeraApp/src/app/components/body/body.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
 })
 
 export class BodyComponent{
+ 
+    mostrar = true;
+
+    frase: any= {
+        mensaje:"Un gran poder requiere un gran responsabilidad",
+        autor: "Petter Parker Ben"
+    }
+
+    personajes: string [] = ['Deadpool','SpiderMan','Iron Man','Hulk'];
+   
+   
 
 }
