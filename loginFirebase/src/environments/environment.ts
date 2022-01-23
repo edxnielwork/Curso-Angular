dad907abfec6142c745d7515ae311bb40bb51b44
edxnielwork <edxnielwork007@gmail.com>
2022-01-23 12:50:25 -0500

    update

---
+++ loginFirebase/src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA9MvmtMqu1eoa-AC3TagUeTg26jFhNSHk",
+    authDomain: "loginfirebase-e35f9.firebaseapp.com",
+    databaseURL: "https://loginfirebase-e35f9-default-rtdb.firebaseio.com",
+    projectId: "loginfirebase-e35f9",
+    storageBucket: "loginfirebase-e35f9.appspot.com",
+    messagingSenderId: "856208725122",
+    appId: "1:856208725122:web:ab9cfa682fce1728f1109b"
+  }
+};
