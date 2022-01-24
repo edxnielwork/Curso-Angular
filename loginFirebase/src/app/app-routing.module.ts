60da265dcdd8116247d792a66c2e48153cab404b
edxnielwork <edxnielwork007@gmail.com>
2022-01-23 23:05:01 -0500

    updated

--- loginFirebase/src/app/app-routing.module.ts
+++ loginFirebase/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  {
+    pathMatch: 'full',
+    path:"",
+    redirectTo:'home'
+  },
+  {
+    path:"login",
+    component: LoginComponent
+  },
+  {
+    path:"register",
+    component: RegisterComponent
+  },
+  {
+    path:"home",
+    component: HomeComponent
+  },
+  {
+    path:"**",
+    component: PageNotFoundComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- loginFirebase/src/app/app.component.ts
+++ loginFirebase/src/app/app.component.ts
@@ -9,36 +9,4 @@ import { AuthService } from './services/auth.service';
 
 export class AppComponent {
 
-  usuario = {
-    email: "",
-    password: ""
-  }
-
-  constructor(private authService: AuthService) {
-    
-  }
-
-  Send() {
-    const { email, password } = this.usuario;
-    this.authService.login(email, password).then(res => {
-      console.log("Registed:",res);
-    })
-  }
-
-  SendGoogle() {
-    const { email, password } = this.usuario;
-    this.authService.loginWithGoogle(email, password).then(res => {
-      console.log("Registed:",res);
-    })
-  }
-
-  // AddUserLogged(){
-  //   this.authService.getUserLogged().subscribe(res => { 
-  //     console.log(res?.email);
-  //   });
-  // }
-
-  LogOut(){
-    this.authService.logout();
-  }
 }
